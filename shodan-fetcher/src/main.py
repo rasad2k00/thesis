965f6571681439a047d3d040cce5cbace2a964e3
Rashad Aliyev <rasad2k@gmail.com>
2023-12-05 02:15:41 +0100

    Added the feature to use multiple keys
    - The keys can be stored in .env file, seperated by colon
    - Multiple shodan clients is generated and used rotationally

--- shodan-fetcher/src/main.py
+++ shodan-fetcher/src/main.py
@@ -1,33 +1,46 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 from logger import setup_logger
 from shodan import Shodan
 
+load_dotenv()
 logger = setup_logger()
 
 
-def search_shodan(client, query):
-    # Make a shodan query
-    result = client.search(query)
-    logger.info(f"Query run: {query}")
+def search_shodan(clients, query):
+    result = []
+    result_count = 1
+    for i in range(result_count):
+        client = clients[i % len(clients)]
+        result.append(client.search(query))
+        logger.info(f"Query run: {query}")
     return result
 
 
-def get_shodan_client():
-    key = os.getenv("SHODAN_KEY")
-    if key is None:
-        logger.info("Please provide your API key!")
+def get_shodan_clients(keys):
+    clients = []
+    for key in keys:
+        client = Shodan(key)
+        clients.append(client)
+    return clients
+
+
+def read_keys():
+    keys = os.getenv("SHODAN_KEYS")
+    if keys is None:
+        logger.fatal("No keys loaded!")
         sys.exit(1)
-    client = Shodan(key)
-    return client
+    return keys.split(":")
 
 
 def main():
-    client = get_shodan_client()
+    keys = read_keys()
+    clients = get_shodan_clients(keys)
     query = "country:hu"
-    result = search_shodan(client, query)
-    print(f"[*] Result: {result}")
+    result = search_shodan(clients, query)
+    print(f"Result: {result}")
 
 
 if __name__ == "__main__":
